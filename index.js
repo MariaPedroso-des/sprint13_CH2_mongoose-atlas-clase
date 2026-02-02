3699bbb767f3de3057fce17564ae5892dd289756
MariaPedroso-des <marhiapm@gmail.com>
2026-02-02 21:46:55 +0100

    get3

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const app = express()
 const PORT = 8080
 const { dbConnection } = require('./config/config.js')
 const routes = require('./routes')
+const Users = require('./routes/users.js')
 
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/', routes)
+
+app.use('/users', Users)
 
 dbConnection()
 
